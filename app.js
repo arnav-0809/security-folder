b99f3939f72aa45d7dc31c71eeca5f74a4be2629
Arnav Sehgal <arnavsehgal2001@gmail.com>
2021-05-23 16:09:03 +0530

    using google authentication

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const bodyParser = require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
 const encrypt=require("mongoose-encryption");
+const md5=require("md5");
+const bcrypt=require("bcrypt");
+const saltRounds=10;
 
 const app = express();
 
@@ -24,7 +27,6 @@ const userSchema=new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields:["password"]});
 
 const User=new mongoose.model("User",userSchema);
 
@@ -41,18 +43,22 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
-  const newUser=new User({
-    email:req.body.username,
-    password:req.body.password
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser=new User({
+      email:req.body.username,
+      password:hash
+    });
+  
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }else{
+        res.render("secrets");
+      }
+    });
   });
 
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("secrets");
-    }
-  });
 
 });
 
@@ -66,11 +72,15 @@ app.post("/login",function(req,res){
       console.log(err);
     }else{
       if(foundUser){
-        if(foundUser.password===password){
-          res.render("secrets");
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+           if(result == true)
+           {
+            res.render("secrets");
+           }
+      });
+          
         }
       }
-    }
 
   })
 })
